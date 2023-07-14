19667dee60be5d68bb68b42df8fe68fb138a7e27
Jakub Serwatka <kubaserw@gmail.com>
2023-07-14 13:35:37 +0200

    initial commit

---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,21 @@
+const Popup = () => {
+  return (
+    <div>
+      <header>
+        <p class="font-bold">
+          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
+        </p>
+        <a
+          class="text-lg text-red-400"
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn What is test
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { crx } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import manifest from "./src/manifest";
+
+const isDev = process.env.__DEV__ === "true";
+
+export default defineConfig({
+  plugins: [solidPlugin(), crx({ manifest })],
+  build: {
+    emptyOutDir: true,
+    outDir: "build",
+    sourcemap: isDev,
+    rollupOptions: {
+      output: {
+        entryFileNames: "src/pages/[name]/index.js",
+        chunkFileNames: isDev
+          ? "assets/js/[name].js"
+          : "assets/js/[name].[hash].js",
+      },
+    },
+  },
+});
