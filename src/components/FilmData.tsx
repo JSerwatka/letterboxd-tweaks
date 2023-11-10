445167ae6efb7ebe562e08f320c45cf5e16b6a2c
Jakub Serwatka <kubaserw@gmail.com>
2023-11-10 20:23:30 +0100

    feat: better styling of move data

--- src/components/FilmData.tsx
+++ src/components/FilmData.tsx
@@ -6,8 +6,10 @@ interface FilmDataProps {
 
 const FilmData = (props: FilmDataProps) => {
     return (
-        <div class="absolute text-red-500 left-0 top-0 z-10 text-lg">
-            {props.title} {props.releaseYear} {props.score}
+        <div class="absolute text-black bg-white opacity-60 w-full left-0 top-0 z-10 text-lg">
+            <div>{props.title}</div>
+            <div> {props.releaseYear}</div>
+            <div>{props.score}</div>
         </div>
     );
 };
--- src/manifest.ts
+++ src/manifest.ts
@@ -1,34 +1,34 @@
 import { defineManifest } from '@crxjs/vite-plugin';
 
 const manifest = defineManifest({
-  manifest_version: 3,
-  name: 'Letterboxd Tweaks',
-  version: '0.0.0',
-  description: '',
-  permissions: ['storage'],
-  icons: {
-    '128': 'icons/128x128.png'
-  },
-  background: {
-    service_worker: 'src/pages/background/index.ts',
-    type: 'module'
-  },
-  action: {
-    default_popup: 'src/pages/popup/index.html',
-    default_icon: 'icons/34x34.png'
-  },
-  content_scripts: [
-    {
-      matches: ['http://*.letterboxd.com/*', 'https://*.letterboxd.com/*'],
-      js: ['src/pages/content/index.ts']
-    }
-  ],
-  web_accessible_resources: [
-    {
-      resources: ['assets/js/*.js', 'assets/css/*.css', 'assets/img/*'],
-      matches: ['*://*/*']
-    }
-  ]
+    manifest_version: 3,
+    name: 'Letterboxd Tweaks',
+    version: '0.0.1',
+    description: '',
+    permissions: ['storage'],
+    icons: {
+        '128': 'icons/128x128.png'
+    },
+    background: {
+        service_worker: 'src/pages/background/index.ts',
+        type: 'module'
+    },
+    action: {
+        default_popup: 'src/pages/popup/index.html',
+        default_icon: 'icons/34x34.png'
+    },
+    content_scripts: [
+        {
+            matches: ['http://*.letterboxd.com/*', 'https://*.letterboxd.com/*'],
+            js: ['src/pages/content/index.ts']
+        }
+    ],
+    web_accessible_resources: [
+        {
+            resources: ['assets/js/*.js', 'assets/css/*.css', 'assets/img/*'],
+            matches: ['*://*/*']
+        }
+    ]
 });
 
 export default manifest;
