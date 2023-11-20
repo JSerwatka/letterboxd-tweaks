907298b50200cd7b72a4216ee1451ece103382b6
Jakub Serwatka <jakub.serwatka@skelia.com>
2023-11-20 17:58:08 +0100

    refactor: create generic navbar actions function

--- src/options/navbar.ts
+++ src/options/navbar.ts
@@ -1,90 +1,137 @@
 import { waitForElement } from "@utils/element-observers";
-
-const ACCOUNT_MENU_SELECTOR = ".main-nav .js-nav-account .subnav";
-const PROFILE_MENU_SELECTOR = "nav.profile-navigation";
-const NAVBAR_MENU_SELECTOR = ".main-nav .navitems";
-
-// TODO config for every function should be passed through arguments, structure like this { toHide: ["Home", ...], toRedirect: { <name>: <redirect> }, toRename: { <name>: <new_name> } }
+import { link } from "fs";
 
 type LinkSelectorConfig = { linkSelector: string; menuItemSelector: string };
 type NavLinksSelectors = Record<string, LinkSelectorConfig>;
 
-export async function accountMenuActions(): Promise<void> {
-    const MenuLinks = {
-        Home: {
-            linkSelector: "/",
-            menuItemSelector: "li"
-        },
-        Diary: {
-            linkSelector: "/diary",
-            menuItemSelector: "li"
-        },
-        Reviews: {
-            linkSelector: "/reviews",
-            menuItemSelector: "li"
-        },
-        Likes: {
-            linkSelector: "/likes",
-            menuItemSelector: "li"
-        },
-        Tags: {
-            linkSelector: "/tags",
-            menuItemSelector: "li"
-        },
-        Networks: {
-            linkSelector: "/following",
-            menuItemSelector: "li"
-        },
-        Subscriptions: {
-            linkSelector: "/subscriptions",
-            menuItemSelector: "li"
-        }
-    } as const satisfies NavLinksSelectors;
+// MenuLinks
+const ACCOUNT_MENU_SELECTOR = ".main-nav .js-nav-account .subnav";
+const MenuLinks = {
+    Home: {
+        linkSelector: "/",
+        menuItemSelector: "li"
+    },
+    Diary: {
+        linkSelector: "/diary",
+        menuItemSelector: "li"
+    },
+    Reviews: {
+        linkSelector: "/reviews",
+        menuItemSelector: "li"
+    },
+    Likes: {
+        linkSelector: "/likes",
+        menuItemSelector: "li"
+    },
+    Tags: {
+        linkSelector: "/tags",
+        menuItemSelector: "li"
+    },
+    Networks: {
+        linkSelector: "/following",
+        menuItemSelector: "li"
+    },
+    Subscriptions: {
+        linkSelector: "/subscriptions",
+        menuItemSelector: "li"
+    }
+} as const satisfies NavLinksSelectors;
 
+// ProfileLinks
+const PROFILE_MENU_SELECTOR = "nav.profile-navigation";
+const ProfileLinks = {
+    Diary: {
+        linkSelector: "/diary",
+        menuItemSelector: "li"
+    },
+    Reviews: {
+        linkSelector: "/reviews",
+        menuItemSelector: "li"
+    },
+    Likes: {
+        linkSelector: "/likes",
+        menuItemSelector: "li"
+    },
+    Tags: {
+        linkSelector: "/tags",
+        menuItemSelector: "li"
+    },
+    Networks: {
+        linkSelector: "/following",
+        menuItemSelector: "li"
+    },
+    Invitations: {
+        linkSelector: "/invitations",
+        menuItemSelector: "li"
+    }
+} as const satisfies NavLinksSelectors;
+
+// NavbarLinks
+const NAVBAR_MENU_SELECTOR = ".main-nav .navitems";
+const NavbarLinks = {
+    Activity: {
+        linkSelector: "/activity",
+        menuItemSelector: "li.main-nav-activity"
+    },
+    Members: {
+        linkSelector: "/members",
+        menuItemSelector: "li.main-nav-people"
+    },
+    Journal: {
+        linkSelector: "/journal",
+        menuItemSelector: "li.main-nav-journal"
+    },
+    Films: {
+        linkSelector: "/films",
+        menuItemSelector: "li.films-page"
+    }
+} as const satisfies NavLinksSelectors;
+
+interface NavbarActionsConfig<T extends keyof NavLinksSelectors> {
+    toHide?: T[];
+    toRedirect?: Record<T, { redirectTo: string }>;
+    toRename?: Record<T, { renameTo: string }>;
+}
+
+export async function accountMenuActions(config: NavbarActionsConfig<keyof typeof MenuLinks>): Promise<void> {
     const accountMenu = await waitForElement(document, ACCOUNT_MENU_SELECTOR);
+    if (!accountMenu) return;
 
-    if (accountMenu) {
-        // remove
-        removeNavbarItems(accountMenu, Object.values(MenuLinks));
+    if (config.toHide && config.toHide.length >= 1) {
+        const linksFilteredToHide = Object.entries(MenuLinks)
+            .filter(([linkName, linkSelectorConfig]) => config.toHide!.includes(linkName as keyof typeof MenuLinks))
+            .map(([linkName, linkSelectorConfig]) => linkSelectorConfig);
+        removeNavbarItems(accountMenu, linksFilteredToHide);
+    }
 
-        // redirect + rename
-        const accountFilmsLink = getLinkByHref(accountMenu, "li", "/films");
+    if (config.toRedirect && Object.keys(config.toRedirect)) {
+        const linksFilteredToRedirect = Object.entries(MenuLinks)
+            .filter(([linkName, linkSelectorConfig]) => Object.keys(config.toRedirect!).includes(linkName))
+            .map(([linkName, linkSelectorConfig]) => {
+                return {
+                    linkSelectorConfig: linkSelectorConfig,
+                    redirectTo: config.toRedirect![linkName as keyof typeof MenuLinks].redirectTo
+                };
+            });
 
-        if (accountFilmsLink) {
-            accountFilmsLink.textContent = "Watched";
-            redirectNavLink(accountFilmsLink, "size/large");
-        }
+        redirectNavbarItems(accountMenu, linksFilteredToRedirect);
+    }
+
+    if (config.toRename && Object.keys(config.toRename).length >= 1) {
+        const linksFilteredToRename = Object.entries(MenuLinks)
+            .filter(([linkName, linkSelectorConfig]) => Object.keys(config.toRename!).includes(linkName))
+            .map(([linkName, linkSelectorConfig]) => {
+                return {
+                    linkSelectorConfig: linkSelectorConfig,
+                    renameTo: config.toRename![linkName as keyof typeof MenuLinks].renameTo
+                };
+            });
+
+        renameNavbarItems(accountMenu, linksFilteredToRename);
     }
 }
 
-export async function profileMenuActions(): Promise<void> {
-    const ProfileLinks = {
-        Diary: {
-            linkSelector: "/diary",
-            menuItemSelector: "li"
-        },
-        Reviews: {
-            linkSelector: "/reviews",
-            menuItemSelector: "li"
-        },
-        Likes: {
-            linkSelector: "/likes",
-            menuItemSelector: "li"
-        },
-        Tags: {
-            linkSelector: "/tags",
-            menuItemSelector: "li"
-        },
-        Networks: {
-            linkSelector: "/following",
-            menuItemSelector: "li"
-        },
-        Invitations: {
-            linkSelector: "/invitations",
-            menuItemSelector: "li"
-        }
-    } as const satisfies NavLinksSelectors;
-
+export async function profileMenuActions(config: NavbarActionsConfig<keyof typeof ProfileLinks>): Promise<void> {
     const profileMenu = await waitForElement(document, PROFILE_MENU_SELECTOR);
 
     if (profileMenu) {
@@ -101,26 +148,7 @@ export async function profileMenuActions(): Promise<void> {
     }
 }
 
-export async function navbarMenuActions(): Promise<void> {
-    const NavbarLinks = {
-        Activity: {
-            linkSelector: "/activity",
-            menuItemSelector: "li.main-nav-activity"
-        },
-        Members: {
-            linkSelector: "/members",
-            menuItemSelector: "li.main-nav-people"
-        },
-        Journal: {
-            linkSelector: "/journal",
-            menuItemSelector: "li.main-nav-journal"
-        },
-        Films: {
-            linkSelector: "/films",
-            menuItemSelector: "li.films-page"
-        }
-    } as const satisfies NavLinksSelectors;
-
+export async function navbarMenuActions(config: NavbarActionsConfig<keyof typeof NavbarLinks>): Promise<void> {
     const navbar = await waitForElement(document, NAVBAR_MENU_SELECTOR);
 
     if (navbar) {
@@ -144,6 +172,38 @@ function removeNavbarItems(baseMenuElement: Element, linkSelectorConfigs: LinkSe
             linkSelectoConfig.linkSelector
         )?.parentElement;
         menuItem?.remove();
+    }
+}
+
+function redirectNavbarItems(
+    baseMenuElement: Element,
+    linkRedirectConfigs: { linkSelectorConfig: LinkSelectorConfig; redirectTo: string }[]
+) {
+    for (const { linkSelectorConfig, redirectTo } of linkRedirectConfigs) {
+        const linkElement = getLinkByHref(
+            baseMenuElement,
+            linkSelectorConfig.menuItemSelector,
+            linkSelectorConfig.linkSelector
+        );
+        if (!linkElement) return;
+
+        redirectNavLink(linkElement, redirectTo);
+    }
+}
+
+function renameNavbarItems(
+    baseMenuElement: Element,
+    linkRenameConfigs: { linkSelectorConfig: LinkSelectorConfig; renameTo: string }[]
+) {
+    for (const { linkSelectorConfig, renameTo } of linkRenameConfigs) {
+        const linkElement = getLinkByHref(
+            baseMenuElement,
+            linkSelectorConfig.menuItemSelector,
+            linkSelectorConfig.linkSelector
+        );
+        if (!linkElement) return;
+
+        linkElement.textContent = renameTo;
     }
 }
 
