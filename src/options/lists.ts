98bf2b7535087eb5cd95f85f3e45ba2cc46fff71
Jakub Serwatka <jakub.serwatka@skelia.com>
2023-11-21 15:39:08 +0100

    feat: create option to add movie to private list by default

--- src/options/lists.ts
+++ src/options/lists.ts
@@ -7,3 +7,19 @@ export async function makeNewListPrivate() {
     selectedListOption?.removeAttribute("selected");
     privateListOption?.setAttribute("selected", "selected");
 }
+
+export async function addMovieToPrivateList() {
+    const addToListModal = await waitForElement(document, "#add-to-a-list-modal");
+    const addToListModalButtons = addToListModal?.querySelector(".js-list-type-toggle > .options");
+    const addToListModalBody = addToListModal?.querySelector(".js-add-to-list-body");
+
+    const publicButton = addToListModalButtons?.querySelector("button[data-list-type='public']");
+    const privateButton = addToListModalButtons?.querySelector("button[data-list-type='private']");
+    publicButton?.classList.remove("-selected");
+    privateButton?.classList.add("-selected");
+
+    const publicSelection = addToListModalBody?.querySelector("div[data-list-type='public']");
+    const privateSelection = addToListModalBody?.querySelector("div[data-list-type='private']");
+    publicSelection?.classList.remove("-selected");
+    privateSelection?.classList.add("-selected");
+}
