7a0c5968b36635c4cedbc9fce1f80b3ac3313233
Jakub Serwatka <kubaserw@gmail.com>
2023-11-24 11:35:26 +0100

    feat: add loading icon when featching data

--- src/options/search.tsx
+++ src/options/search.tsx
@@ -1,73 +1,84 @@
-import { createResource, createSignal, on, onMount } from "solid-js";
+import { createEffect, createResource, createSignal, on, onMount } from "solid-js";
 import { onCleanup } from "solid-js";
 import { waitForElement } from "@utils/element-observers";
 import { render } from "solid-js/web";
 
-async function fetchMovies(source: any) {
-    console.log(source);
-    // // abort the previous request if it's still ongoing
-    // if (controller && !controller.signal.aborted) {
-    //     controller.abort();
-    // }
-
-    // controller = new AbortController();
-    // const signal = controller.signal;
-
-    // const userInput = searchField.value;
-    // let response;
-
-    // try {
-    //     response = await fetch(
-    //         `https://letterboxd.com/s/autocompletefilm?q=${userInput}&limit=10&timestamp=1700657512497&adult=false`,
-    //         { signal }
-    //     );
-    // } catch (err) {
-    //     // ignore abort error
-    //     if (err instanceof Error && err.name !== "AbortError") {
-    //     }
-    // }
-
-    // if (!response) {
-    //     return;
-    // }
-
-    // const movieList = await response.json();
-
-    // const movieListMapped = await Promise.all(
-    //     movieList.data.map(async (movie: any) => {
-    //         const posterPageResponse = await fetch(`https://letterboxd.com/ajax/poster${movie.url}std/110x165/`, {
-    //             signal
-    //         });
-    //         const posterPageText = await posterPageResponse.text();
-
-    //         const parser = new DOMParser();
-    //         const posterPageHTML = parser.parseFromString(posterPageText, "text/html");
-
-    //         const posterImgElement = posterPageHTML.querySelector("img.image:not(.empty-poster-image)") as
-    //             | HTMLImageElement
-    //             | null
-    //             | undefined;
-
-    //         return {
-    //             url: movie.url,
-    //             name: movie.name,
-    //             releaseYear: movie.releaseYear,
-    //             directors: movie.directors,
-    //             poster: posterImgElement?.src
-    //         };
-    //     })
-    // );
+interface MovieSearchResult {
+    url: string;
+    name: string;
+    releaseYear: number;
+    directors: string[];
+    poster: string;
 }
 
-export function SearchAutocomplete() {
-    let controller: AbortController | null = null;
+const [controller, setController] = createSignal<AbortController | null>(null);
+
+async function fetchMovies(userInput: string | null): Promise<MovieSearchResult[] | undefined> {
+    if (!userInput) {
+        return;
+    }
+
+    // abort the previous request if it's still ongoing
+    if (!controller()?.signal.aborted) {
+        controller()?.abort();
+    }
+
+    setController(new AbortController());
+    const signal = controller()?.signal;
+
+    let response;
+
+    try {
+        response = await fetch(
+            `https://letterboxd.com/s/autocompletefilm?q=${userInput}&limit=10&timestamp=1700657512497&adult=false`,
+            { signal }
+        );
+    } catch (err) {
+        // ignore abort error
+        if (err instanceof Error && err.name !== "AbortError") {
+        }
+    }
+
+    if (!response) {
+        return;
+    }
+
+    const movieList = await response.json();
+
+    return Promise.all(
+        movieList.data.map(async (movie: any) => {
+            const posterPageResponse = await fetch(`https://letterboxd.com/ajax/poster${movie.url}std/110x165/`, {
+                signal
+            });
+            const posterPageText = await posterPageResponse.text();
+
+            const parser = new DOMParser();
+            const posterPageHTML = parser.parseFromString(posterPageText, "text/html");
+
+            const posterImgElement = posterPageHTML.querySelector("img.image:not(.empty-poster-image)") as
+                | HTMLImageElement
+                | null
+                | undefined;
+
+            return {
+                url: movie.url,
+                name: movie.name,
+                releaseYear: movie.releaseYear,
+                directors: movie.directors,
+                poster: posterImgElement?.src
+            };
+        })
+    );
+}
+
+export function SearchAutocomplete({ searchFieldsContainer }: { searchFieldsContainer: Element }) {
     let timeoutId: NodeJS.Timeout | null = null;
-    const [searchValue, setSearchValue] = createSignal();
+    const [searchValue, setSearchValue] = createSignal<string | null>(null);
     const [data] = createResource(searchValue, fetchMovies);
+    const searchInputField = searchFieldsContainer.querySelector("input#search-q");
+    const searchIcon = searchFieldsContainer.querySelector("input[type='submit']") as HTMLElement | undefined;
 
     onMount(async () => {
-        const searchInputField = await waitForElement(document, "form#search input#search-q");
-
         searchInputField?.addEventListener("keyup", async (event: Event) => {
             const searchField = event.target as HTMLInputElement;
 
@@ -85,14 +96,34 @@ export function SearchAutocomplete() {
             clearTimeout(timeoutId);
         }
 
-        if (controller && !controller.signal.aborted) {
-            controller.abort();
+        if (!controller()?.signal.aborted) {
+            controller()?.abort();
+        }
+    });
+
+    createEffect(() => {
+        console.log(data());
+    });
+
+    createEffect(() => {
+        if (!searchIcon) return;
+
+        if (data.loading) {
+            searchIcon.style.background =
+                "url(https://s.ltrbxd.com/static/img/spinner-light-2x.3653aae2.gif) no-repeat";
+            searchIcon.style.backgroundPosition = "center";
+            searchIcon.style.backgroundSize = "15px 15px";
+        } else {
+            searchIcon.style.background = "url(https://s.ltrbxd.com/static/img/sprite.91ec427c.svg) no-repeat";
+            searchIcon.style.backgroundPosition = "-100px -170px";
+            searchIcon.style.backgroundSize = "800px 1020px";
         }
     });
 
     return (
         <>
-            <div>hello</div>
+            <div>he</div>
+            {/* <div>{console.log(data())}</div> */}
             {/* <input id="search-q" type="text" placeholder="Search movies..." /> */}
             {/* {loading() && <div>Loading...</div>} */}
             {/* Render movies here */}
@@ -100,6 +131,10 @@ export function SearchAutocomplete() {
     );
 }
 
-export function renderSearch() {
-    return render(() => <SearchAutocomplete />, document);
+export async function renderSearch() {
+    const searchFieldsContainer = await waitForElement(document, "form#search fieldset");
+
+    if (!searchFieldsContainer) return;
+
+    return render(() => <SearchAutocomplete searchFieldsContainer={searchFieldsContainer} />, document);
 }
