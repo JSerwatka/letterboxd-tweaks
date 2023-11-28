8974e5e36f38715ca2f2b966f64324df6cbe3609
Jakub Serwatka <jakub.serwatka@skelia.com>
2023-11-28 14:26:46 +0100

    feat: improve styles of film badge

--- src/components/FilmBadge.tsx
+++ src/components/FilmBadge.tsx
@@ -8,19 +8,19 @@ function getScoreColor(score: FilmDataProps["score"]) {
     const scoreNumber = Number(score)
 
     if (scoreNumber <= 2) {
-        return 'bg-red-500/90';
+        return 'bg-red-500/80';
     }
     if (scoreNumber >= 2 && scoreNumber < 3) {
-        return 'bg-orange-600/90';
+        return 'bg-orange-600/80';
     }
     if (scoreNumber >= 3 && scoreNumber < 3.5) {
-        return 'bg-yellow-500/90';
+        return 'bg-yellow-500/80';
     }
     if (scoreNumber >= 3.5 && scoreNumber < 4) {
-        return 'bg-[#b7dd29]/90';
+        return 'bg-[#b7dd29]/80';
     }
     if (scoreNumber >= 4) {
-        return 'bg-green-500/90';
+        return 'bg-green-500/80';
     }
 }
 
@@ -28,7 +28,7 @@ const FilmBadge = ({ score }: FilmDataProps) => {
     const scoreColor = getScoreColor(score);
 
     return (
-        <div class={`absolute text-black px-[4px] py-[3px] rounded-br-md top-0 ${scoreColor}`}>{score ?? "?"}</div>
+        <div class={`absolute text-black px-[4px] py-[2px] rounded-br-md top-0 ${scoreColor}`}>{score ?? "?"}</div>
     );
 }
 
