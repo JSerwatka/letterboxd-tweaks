d4767772628753194f77dbae6609ea6a2ee14908
Jakub Serwatka <jakub.serwatka@skelia.com>
2023-11-29 18:34:03 +0100

    feat: improve padding small card

--- src/components/FilmData.tsx
+++ src/components/FilmData.tsx
@@ -16,8 +16,8 @@ export const FilmDataLarge = (props: FilmDataProps) => {
 
 export const FilmDataSmall = (props: FilmDataProps) => {
     return (
-        <div class="text-white px-8 py-1 w-[200px] left-0 bottom-0 rounded-md text-lg flex flex-col  justify-between">
-            <div class="line-clamp-2 text-lg leading-tigh" title={props.title}>{props.title}</div>
+        <div class="text-white px-8 w-[200px] left-0 bottom-0 rounded-md text-lg flex flex-col  justify-between">
+            <div class="line-clamp-2 text-lg leading-tigh mt-2" title={props.title}>{props.title}</div>
             <div>
                 <div class="text-gray-400">{props.releaseYear}</div>
                 {props.yourRating && <div title="your rating" class="rating -tiny -darker rated-9 mt-3">{props.yourRating}</div>}
