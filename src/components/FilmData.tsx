8b4ddfa958784d962694edcb71bbdbd0ba2e30be
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2023-12-01 11:37:30 +0100

    feat: add date of watch

--- src/components/FilmData.tsx
+++ src/components/FilmData.tsx
@@ -5,13 +5,29 @@ interface FilmDataProps {
     yourRating?: string | null;
 }
 
-export const FilmDataLarge = (props: FilmDataProps) => {
+interface FilmDataLargeProps extends FilmDataProps {
+    dateOfView?: string | null;
+    likedIcon?: Element | null;
+}
+
+export const FilmDataLarge = (props: FilmDataLargeProps) => {
     return (
-        <div class="text-white w-full rounded-md text-lg mt-4 flex flex-col h-20  justify-between">
-            <div class="line-clamp-2 text-lg leading-tight" title={props.title}>{props.title}</div>
+        <div class="text-white w-full rounded-md text-lg mt-4 flex flex-col h-28  justify-between">
+            <div class="line-clamp-2 text-lg leading-tight" title={props.title}>
+                {props.title}
+            </div>
             <div>
                 <div class="text-gray-400">{props.releaseYear}</div>
-                {props.yourRating && <div title="your rating" class="rating -tiny -darker rated-9 mt-3">{props.yourRating}</div>}
+                {props.yourRating && (
+                    <div class="flex flex-row justify-between items-end mt-3">
+                        <div title="your rating" class="rating -tiny -darker rated-9">
+                            {props.yourRating}
+                        </div>
+                        <div title="viewing date" class="text-gray-400 text-sm">
+                            {props.dateOfView}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
@@ -20,10 +36,16 @@ export const FilmDataLarge = (props: FilmDataProps) => {
 export const FilmDataSmall = (props: FilmDataProps) => {
     return (
         <div class="text-white px-8 w-[200px] left-0 bottom-0 rounded-md text-lg flex flex-col  justify-between">
-            <div class="line-clamp-2 text-lg leading-tigh mt-2" title={props.title}>{props.title}</div>
+            <div class="line-clamp-2 text-lg leading-tigh mt-2" title={props.title}>
+                {props.title}
+            </div>
             <div>
                 <div class="text-gray-400">{props.releaseYear}</div>
-                {props.yourRating && <div title="your rating" class="rating -tiny -darker rated-9 mt-3">{props.yourRating}</div>}
+                {props.yourRating && (
+                    <div title="your rating" class="rating -tiny -darker rated-9 mt-3">
+                        {props.yourRating}
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/options/films.tsx
+++ src/options/films.tsx
@@ -37,8 +37,11 @@ const getScore = async (film: HTMLElement) => {
 };
 
 export const showFilmData = async () => {
+    // TODO: refactor
+    // TODO: in watched there is also a date of watch - move to card
+    // TODO: in new from friends (home) there is also type of watch and who watched that
     const filmListContainer = await waitForElement(document, "ul.poster-list");
-    let styleElement = document.createElement('style');
+    let styleElement = document.createElement("style");
     styleElement.innerHTML = `
         ul.poster-list.no-after::after {
             content: none !important;
@@ -73,12 +76,14 @@ export const showFilmData = async () => {
         const title = film.dataset.filmName;
 
         film.style.position = "relative";
-        const yourRatingElement = film?.parentElement?.querySelector("p.poster-viewingdata > span.rating");
-        const yourRatingStars = yourRatingElement?.textContent;
-        yourRatingElement?.remove();
+        const viewingDataElement = film?.parentElement?.querySelector("p.poster-viewingdata");
+        const yourRatingStars = viewingDataElement?.querySelector("span.rating")?.textContent;
+        const dateOfView = viewingDataElement?.querySelector("time")?.textContent; //only large cards
+        const commentsIcon = viewingDataElement?.querySelector(".review-small > span.icon"); //only large cards
+        const likedIcon = viewingDataElement?.querySelector(".icon-liked > span.icon"); //only large cards
+        viewingDataElement?.remove();
 
         const isSmallCard = false;
-
 
         if (isSmallCard) {
             film.style.display = "flex";
@@ -86,7 +91,7 @@ export const showFilmData = async () => {
             film.style.boxShadow = "none";
             film.style.borderRadius = "8px";
             film.style.background = "#7eb4f121";
-            film.style.backdropFilter = "blur(10px)"
+            film.style.backdropFilter = "blur(10px)";
 
             const aTag = film.querySelector("a.frame") as HTMLElement;
             aTag.style.boxShadow = "none";
@@ -101,24 +106,24 @@ export const showFilmData = async () => {
             const overlayActions = film.querySelector("span.overlay-actions") as HTMLElement;
             overlayActions.style.zIndex = "20";
 
-            let imgElement = film.querySelector('img.image') as HTMLElement;
+            let imgElement = film.querySelector("img.image") as HTMLElement;
             if (imgElement) {
-                imgElement.style.boxShadow = "0px 0px 7px 0px rgb(0 0 0 / 50%)"
+                imgElement.style.boxShadow = "0px 0px 7px 0px rgb(0 0 0 / 50%)";
                 imgElement.style.borderRadius = "6px";
 
                 let clonedImg = imgElement.cloneNode(true) as HTMLElement;
-                clonedImg.classList.add("blurred-img-overlay")
+                clonedImg.classList.add("blurred-img-overlay");
                 imgElement.after(clonedImg);
             }
 
-            render(() => <FilmBadge score={score} isColorfulBadge={true} />, film)
+            render(() => <FilmBadge score={score} isColorfulBadge={true} />, film);
             render(() => <FilmDataSmall title={title} releaseYear={releaseYear} yourRating={yourRatingStars} />, film);
         } else {
             film.style.padding = "12px";
             film.style.boxShadow = "none";
             film.style.borderRadius = "8px";
             film.style.background = "#7eb4f121";
-            film.style.backdropFilter = "blur(10px)"
+            film.style.backdropFilter = "blur(10px)";
             film.style.height = "auto";
 
             const aTag = film.querySelector("a.frame") as HTMLElement;
@@ -133,39 +138,51 @@ export const showFilmData = async () => {
 
             const overlayActions = film.querySelector("span.overlay-actions") as HTMLElement;
             overlayActions.style.zIndex = "20";
+            // overlayActions.appendChild(commentsIcon as Node);
 
-            let imgElement = film.querySelector('img.image') as HTMLElement;
+            let imgElement = film.querySelector("img.image") as HTMLElement;
             if (imgElement) {
-                imgElement.style.boxShadow = "0px 0px 7px 0px rgb(0 0 0 / 50%)"
+                imgElement.style.boxShadow = "0px 0px 7px 0px rgb(0 0 0 / 50%)";
                 imgElement.style.borderRadius = "6px";
 
                 let clonedImg = imgElement.cloneNode(true) as HTMLElement;
-                clonedImg.classList.add("blurred-img-overlay-large")
+                clonedImg.classList.add("blurred-img-overlay-large");
                 imgElement.after(clonedImg);
             }
 
-            render(() => <FilmBadge score={score} isColorfulBadge={true} />, film)
-            render(() => <FilmDataLarge title={title} releaseYear={releaseYear} score={score} yourRating={yourRatingStars} />, film);
+            render(() => <FilmBadge score={score} isColorfulBadge={true} />, film);
+            render(
+                () => (
+                    <FilmDataLarge
+                        title={title}
+                        releaseYear={releaseYear}
+                        score={score}
+                        yourRating={yourRatingStars}
+                        dateOfView={dateOfView}
+                        likedIcon={likedIcon}
+                    />
+                ),
+                film
+            );
         }
     });
 };
 
-// WARNING: because there is no other selector, I have to doublecheck if the lable inside is "Service", 
+// WARNING: because there is no other selector, I have to doublecheck if the lable inside is "Service",
 // which means that this code will no work if the page is translated
 export async function hideService() {
-    const serviceElement = await waitForElement(document, "div.sorting-selects > section.smenu-wrapper:nth-child(4)")
+    const serviceElement = await waitForElement(document, "div.sorting-selects > section.smenu-wrapper:nth-child(4)");
     const labelElement = serviceElement?.querySelector("label");
 
     if (labelElement?.textContent?.trim() === "Service") {
         serviceElement?.remove();
     }
-
 }
 
 export async function moveMovieDataToHeader() {
     const filmHeaderSection = await waitForElement(document, "#featured-film-header");
 
-    const durationSection = (await waitForElement(document, "p.text-footer"));
+    const durationSection = await waitForElement(document, "p.text-footer");
 
     if (durationSection && durationSection.contains(durationSection.querySelector("[data-track-action='TMDb']"))) {
         const durationSectionText = durationSection?.textContent?.trim();
@@ -182,8 +199,6 @@ export async function moveMovieDataToHeader() {
         filmHeaderSection?.appendChild(p);
     }
 
-
-
     const genreSection = (await waitForElement(document, "#tab-genres a[href*='/films/genre']"))?.parentElement;
 
     if (genreSection) {
@@ -192,7 +207,7 @@ export async function moveMovieDataToHeader() {
 
         genreNames.forEach((genreName) => {
             if (!genreName || !filmHeaderSection) return;
-            render(() => <GenreBadge genreName={genreName} />, filmHeaderSection)
-        })
+            render(() => <GenreBadge genreName={genreName} />, filmHeaderSection);
+        });
     }
 }
