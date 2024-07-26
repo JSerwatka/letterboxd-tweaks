9ccad13d18e9e5f9aaa0bc3302a1ab1d8040f7fb
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2024-07-26 13:55:01 +0200

    feat: load option dynamically

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,34 +1,60 @@
 import "@tailwind";
 import { getPageFromPathname, shouldRunFunctionOnPage } from "@utils/page-lookup";
 import { StorageSelectedOptions } from "@utils/chrome-storage";
+import { defaultOptions, Section } from "@configs/default-options";
 
 // TODO run functions of all options in storage
 
 // console log all options in storage
 
-chrome.storage.onChanged.addListener((changes, areaName) => {
-    console.log(changes, areaName);
-    if (areaName === "sync") {
-        console.log("local storage has changed", changes);
-        // TODO run given option's function
+// chrome.storage.onChanged.addListener((changes, areaName) => {
+//     console.log(changes, areaName);
+//     if (areaName === "sync") {
+//         // console.log("local storage has changed", changes);
+//         // TODO run given option's function
+//     }
+// });
+
+const loadImport = (section: Section) => {
+    switch (section) {
+        case "films":
+            return import("@options/films/films");
+        case "filter":
+            return import("@options/filter/filter");
+        case "lists":
+            return import("@options/lists/lists");
+        case "navbar":
+            return import("@options/navbar/navbar");
+        case "search":
+            return import("@options/search/search");
+        case "sort":
+            return import("@options/sort/sort");
+        default:
+            console.error(`Section ${section} not found`);
     }
-});
+};
 
 async function main(): Promise<void> {
     console.log("options");
     const currentPageName = getPageFromPathname(window.location.pathname);
+    // await import("@options/films/filmsContainer");
+    // console.log({currentPageName});
+    // console.log({ shouldRunFunctionOnPage: shouldRunFunctionOnPage(currentPageName, "showFilmData") });
 
-    console.log({ shouldRunFunctionOnPage: shouldRunFunctionOnPage(currentPageName, "showFilmData") });
+    chrome.storage.sync.get(null, async (userSelectedOptions: StorageSelectedOptions) => {
+        for (const [optionId] of Object.entries(userSelectedOptions)) {
+            const checkedOptionFullData = defaultOptions.find(option => option.id === optionId);
+            if (!checkedOptionFullData) {
+                console.error(`Option with id ${optionId} not found`);
+                continue;
+            }
 
-    chrome.storage.sync.get(null, (userSelectedOptions: StorageSelectedOptions) => {
-        for (const [optionId, isChecked] of Object.entries(userSelectedOptions)) {
-            console.log(optionId, isChecked);
+            // TODO: remove any
+            const functionFile = await loadImport(checkedOptionFullData.section) as any;
+            functionFile[checkedOptionFullData.function]();
         }
     });
 
-    // using window is the easiest way to access pathname
-    // console.log(window.location.pathname);
-    // console.log(checkIfOptionPage(["diary", "filmSingle"], true));
     // const file = await import("@options/films");
     // file["showFilmData"]();
     // await import("@options/films/filmsContainer");
