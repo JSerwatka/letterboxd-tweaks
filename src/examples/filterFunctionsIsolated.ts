974e79887c0e87d74d0be3a2b0652d64913a4a26
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2024-08-05 12:05:03 +0200

    refactor: comment out not used code

--- src/examples/filterFunctionsIsolated.ts
+++ src/examples/filterFunctionsIsolated.ts
@@ -1,44 +1,44 @@
-import { getPageFromPathname, shouldRunFunctionOnPage } from "@utils/page-lookup";
-import { AdditionalOptionConfigType, ContentFilterConfigType, AccountFilterConfigType, hideAdditionalOptions, hideContentFilters, hideAccountFilters } from "@options/filter/filter";
+// import { getPageFromPathname, shouldRunFunctionOnPage } from "@utils/page-lookup";
+// import { AdditionalOptionConfigType, ContentFilterConfigType, AccountFilterConfigType, hideAdditionalOptions, hideContentFilters, hideAccountFilters } from "@options/filter/filter";
 
 
-const CONTENT_FILTER_CONFIG_DEFAULT: ContentFilterConfigType = {
-    toHide: [
-        "Hide documentaries",
-        "Hide unreleased titles"
-    ]
-};
+// const CONTENT_FILTER_CONFIG_DEFAULT: ContentFilterConfigType = {
+//     toHide: [
+//         "Hide documentaries",
+//         "Hide unreleased titles"
+//     ]
+// };
 
-const ADDITIONAL_OPTIONS_CONFIG_DEFAULT: AdditionalOptionConfigType = {
-    toHide: [
-        "Fade watched movies", 
-        "Show custom posters"
-    ]
-};
+// const ADDITIONAL_OPTIONS_CONFIG_DEFAULT: AdditionalOptionConfigType = {
+//     toHide: [
+//         "Fade watched movies", 
+//         "Show custom posters"
+//     ]
+// };
 
-const ACCOUNT_FILTER_CONFIG_DEFAULT: AccountFilterConfigType = {
-    toHide: [
-        "Show/hide watched movies",
-        "Show/hide liked movies",
-        "Show/hide reviewed films",
-        "Show/hide rewatched films",
-        "Show/hide logged films",
-        "Show/hide rated films",
-        "Show/hide films in watchlist"
-    ]
-};
+// const ACCOUNT_FILTER_CONFIG_DEFAULT: AccountFilterConfigType = {
+//     toHide: [
+//         "Show/hide watched movies",
+//         "Show/hide liked movies",
+//         "Show/hide reviewed films",
+//         "Show/hide rewatched films",
+//         "Show/hide logged films",
+//         "Show/hide rated films",
+//         "Show/hide films in watchlist"
+//     ]
+// };
 
-export const run = () => {
-    if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideAdditionalOptions")) {
-        hideAdditionalOptions(ADDITIONAL_OPTIONS_CONFIG_DEFAULT);
-    }
+// export const run = () => {
+//     if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideAdditionalOptions")) {
+//         hideAdditionalOptions(ADDITIONAL_OPTIONS_CONFIG_DEFAULT);
+//     }
     
-    if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideAccountFilters")) {
-        hideAccountFilters(ACCOUNT_FILTER_CONFIG_DEFAULT);
-    }
+//     if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideAccountFilters")) {
+//         hideAccountFilters(ACCOUNT_FILTER_CONFIG_DEFAULT);
+//     }
     
-    if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideContentFilters")) {
-        hideContentFilters(CONTENT_FILTER_CONFIG_DEFAULT);
-    }   
-}
+//     if (shouldRunFunctionOnPage(getPageFromPathname(window.location.pathname), "hideContentFilters")) {
+//         hideContentFilters(CONTENT_FILTER_CONFIG_DEFAULT);
+//     }   
+// }
 
