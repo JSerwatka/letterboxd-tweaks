d2ee536179daa943e07d203d942b4c35cbbe4bbc
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2024-08-07 13:09:54 +0200

    feat: add manifest description

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,11 +3,14 @@ import { defineManifest } from "@crxjs/vite-plugin";
 const manifest = defineManifest({
     manifest_version: 3,
     name: "Letterboxd Tweaks",
+    description: "Enhances the Letterboxd website with cleaner movie cards, a more efficient search bar, and an improved user interface that hides unnecessary filters, navbar items, and sort options",
     version: "0.0.1",
-    description: "",
     permissions: ["storage", "activeTab"],
     icons: {
-        "128": "icons/128x128.png"
+        '16': 'img/logo-16.png',
+        '32': 'img/logo-32.png',
+        '48': 'img/logo-48.png',
+        '128': 'img/logo-128.png'
     },
     background: {
         service_worker: "src/pages/background/index.ts",
@@ -23,7 +26,13 @@ const manifest = defineManifest({
             matches: ["http://*.letterboxd.com/*", "https://*.letterboxd.com/*"],
             js: ["src/pages/content/index.ts"]
         }
-    ]
+    ],
+    web_accessible_resources: [
+        {
+          resources: ['icons/logo-16.png', 'icons/logo-32.png', 'icons/logo-48.png', 'icons/logo-128.png'],
+          matches: []
+        }
+      ]
 });
 
 export default manifest;
