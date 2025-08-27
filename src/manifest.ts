a56620a9da5e47ab3c3f3d9ae21343cbb6051fdf
Jakub Serwatka <jakub.serwatka@skelia.com>
2025-08-27 13:31:04 +0200

    chore: update project version and time

--- src/manifest.ts
+++ src/manifest.ts
@@ -10,7 +10,7 @@ const manifestBase: ManifestV3Export = {
     name: "Letterboxd Tweaks",
     description:
         "Enhance Letterboxd with cleaner movie cards, instant search suggestions, and various quality of life improvements.",
-    version: "0.0.12",
+    version: "0.0.13",
     permissions: ["storage"],
     host_permissions: [
         "https://letterboxd.com/*",
