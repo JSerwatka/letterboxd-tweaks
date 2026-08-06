33a11f8ee69c169415290105c690913e747356bd
Jakub Serwatka <jserwatka.dev@gmail.com>
2026-08-06 18:55:19 +0200

    fix: default options reenable on update (#76)

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,6 +1,8 @@
 import { defaultOptions } from "@configs/default-options";
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason !== "install") return;
+
     defaultOptions.forEach((option) => {
         option.checked && chrome.storage.sync.set({ [option.id]: option.checked });
     });
